0bf3d7c18bba859cf31b82bb6110c3e355b34a9f
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-01-09 15:20:08 -0300

    add_darkMode&Favorites

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,27 +1,63 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useReducer} from 'react'
 import "./Character.css"
+
+const initialState = {
+    favorites:[]
+}
+const favoriteReducer = (state, action) => {
+    switch(action.type){
+        case 'ADD_TO_FAVORITE':
+            return{
+                ...state,
+                favorites:[...state.favorites, action.payload]
+            }
+            default:
+                return state;
+    }
+}
+
+
 function Characters() {
     const [characters, setCharacters] = useState([])
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState) 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character/')
-            .then(res => res.json())
-            .then(respuesta => setCharacters(respuesta.results))
+        .then(res => res.json())
+        .then(respuesta => setCharacters(respuesta.results ))
+    
     },[])
-    console.log(characters)
+    const handleClick = favorite => {
+        dispatch({ type:'ADD_TO_FAVORITE', payload:favorite})
+    }
+
+   
+
     return (
-        <div className="characters-container">
-            {characters.map(({name,gender,species,image }) => 
-            <div className="character-card">
-                <h2>{name}</h2>
-                <img src={image} alt={name}/>
-                <div className="desc">
-                    <p>Sprecie: {species === 'Human' ? "Human 🙍‍♂️" : "Alien 👽" }</p>
-                    <p>Gender: {gender}</p>
-                </div>
+        <div className="container">
+            <div className="favorite-container">
+                {favorites.favorites.map(fav => (
+                    <div className="fav">
+                        <img src={fav} alt=""/>
+                    </div>
+                ))}
             </div>
-            )}
+            <div className="characters-container">
+                {characters.map(({name,gender,species,image }) => 
+                    <div className="character-card">
+                        <h2>{name}</h2>
+                        <img src={image} alt={name}/>
+                        <div className="desc">
+                            <p>Sprecie: {species === 'Human' ? "Human 🙍‍♂️" : "Alien 👽" }</p>
+                            <p>Gender: {gender}</p>
+                        </div>
+                    <button type="button" onClick={() => handleClick(image)} >🌟</button>
+                    </div>
+                )}
+            </div>
         </div>
+        
     )
 }
+
 
 export default Characters
